865ffa755ca52c1b0dbc1ce7f6cec7874f889239
upadhyayaditi <au.aditi08@gmail.com>
2024-04-19 10:05:54 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/
 import { app } from "../firebase"
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice"
 import { useDispatch } from "react-redux"
+import { Link } from "react-router-dom"
 
 const Profile = () => {
   const {currentUser, loading, error} = useSelector((state) => state.user)
@@ -97,12 +98,12 @@ const Profile = () => {
       const res = await fetch('/api/auth/signout');
       const data = await res.json();
       if (data.success === false) {
-        dispatch(deleteUserFailure(data.message));
+        dispatch(signOutUserFailure(data.message));
         return;
       }
-      dispatch(deleteUserSuccess(data));
+      dispatch(signOutUserSuccess(data));
     } catch (error) {
-      dispatch(deleteUserFailure(data.message));
+      dispatch(signOutUserFailure(data.message));
     }
   };
 
@@ -167,6 +168,12 @@ const Profile = () => {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+
+        <Link 
+        className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95 "
+        to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
 
       <div className="flex justify-between mt-5 ">
